77e6cff831781f6383a0885f1b1e1be54794e313
Dmitry Kotegov <d.kotegov@corp.mail.ru>
2015-11-06 22:37:16 +0300

    new run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import sys
+
+from tests.test_calculator import CalculatorTestCase
 
 if __name__ == '__main__':
-    pass
+    suite = unittest.TestSuite((
+        unittest.makeSuite(CalculatorTestCase),
+    ))
+    result = unittest.TextTestRunner().run(suite)
+    sys.exit(not result.wasSuccessful())
